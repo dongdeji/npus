b3e394468569c3a7912d4ac2de7780576c4d1a8c
dongdeji <dongdeji@126.com>
2021-12-24 11:41:45 +0800

    dongdeji update

--- src/main/scala/Axi4Dev.scala
+++ src/main/scala/Axi4Dev.scala
@@ -143,7 +143,69 @@ class Axi4Tcam(id: Int)(implicit p: Parameters) extends LazyModule with NpusPara
   }
 }
 
+class Axi4Lram(id: Int)(implicit p: Parameters) extends LazyModule with NpusParams
+{
+  val address = AddressSet(tcamBase, tcamSize-1)
+  val slavenode = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
+    Seq(AXI4SlaveParameters(
+      address       = Seq(address),
+      //resources     = resources,
+      regionType    = if (true) RegionType.UNCACHED else RegionType.IDEMPOTENT,
+      executable    = true,
+      supportsRead  = TransferSizes(1, fetchBytes),
+      supportsWrite = TransferSizes(1, fetchBytes),
+      interleavedId = Some(0))),
+    beatBytes  = fetchBytes,
+    requestKeys = if (true) Seq(AMBACorrupt) else Seq(),
+    minLatency = 1)))
 
+  lazy val module = new LazyModuleImp(this) 
+  {
+    val (in, edgeIn) = slavenode.in(0)
+    chisel3.dontTouch(in)
+    val tcam_reg = RegInit(0x72345678.U(32.W)); chisel3.dontTouch(tcam_reg)
+    in.aw.ready := true.B
+    in.w.ready := true.B
+    in.ar.ready := true.B
+    
+    val aw_fire = RegInit(false.B)
+    val ar_fire = RegInit(false.B)
+    val aw_id = RegInit(0.U)
+    val ar_id = RegInit(0.U)
+
+    val IncCounter = Counter(5)
+
+    // handle aw/w->b begin
+    when(in.aw.fire() && address.contains(in.aw.bits.addr) && in.w.fire())
+    { 
+      tcam_reg := in.w.bits.data(31,0) 
+      aw_fire := in.aw.fire()
+      aw_id := in.aw.bits.id
+    }
+    in.b.valid := aw_fire
+    in.b.bits.id := aw_id
+    when(aw_fire && in.b.fire())
+    { aw_fire := false.B }
+    // handle aw/w->b end
+
+    // handle ar->r begin
+    val sel_s0 = address.contains(in.ar.bits.addr); chisel3.dontTouch(sel_s0)
+    when(in.ar.fire() && address.contains(in.ar.bits.addr))
+    { 
+      ar_fire := true.B
+      ar_id := in.aw.bits.id
+      IncCounter.reset
+    }
+
+    in.r.valid := ar_fire && IncCounter.inc
+    in.r.bits.data := ar_id
+    in.r.bits.data := Cat(0.U(33.W), tcam_reg(30,0))
+    when(ar_fire && in.r.fire())
+    { ar_fire := false.B }
+    // handle ar->r end
+
+  }
+}
 
 
 
--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -116,26 +116,28 @@ class npusTop()(implicit p: Parameters) extends LazyModule with NpusParams
   ElaborationArtefacts.add("graphml", graphML)
   ElaborationArtefacts.add("plusArgs", PlusArgArtefacts.serialize_cHeader)
   
-  val fbusmaster = AXI4MasterNode(Seq(AXI4MasterPortParameters(
+  private val fbusmaster = AXI4MasterNode(Seq(AXI4MasterPortParameters(
                                         masters = Seq(AXI4MasterParameters(
                                                         name = s"FrontBuss_src",
                                                         id   = IdRange(0, 1 << 1))))))
-  val windmaster = AXI4MasterNode(Seq(AXI4MasterPortParameters(
+  private val windmaster = AXI4MasterNode(Seq(AXI4MasterPortParameters(
                                         masters = Seq(AXI4MasterParameters(
                                                         name = s"Window_src",
                                                         id   = IdRange(0, 1 << 1))))))
-  val accxbar = LazyModule(new AXI4Xbar)
+  private val accxbar = LazyModule(new AXI4Xbar)
   private val tcam = LazyModule(new Axi4Tcam(0))
+  private val lram = LazyModule(new Axi4Lram(0))
   tcam.slavenode := accxbar.node
+  lram.slavenode := accxbar.node
 
-  val windxbar = LazyModule(new AXI4Xbar)
+  private val windxbar = LazyModule(new AXI4Xbar)
   windxbar.node := windmaster
 
-  val mmioxbar = LazyModule(new AXI4Xbar)
-  val uart = LazyModule(new Axi4Uart(0))
+  private val mmioxbar = LazyModule(new AXI4Xbar)
+  private val uart = LazyModule(new Axi4Uart(0))
   uart.slavenode := mmioxbar.node
   
-  val clusters = Seq.tabulate(numCluster) 
+  private val clusters = Seq.tabulate(numCluster) 
   { i => 
     val cluster = LazyModule(new Cluster(i)) 
     accxbar.node := cluster.accxbar.node
